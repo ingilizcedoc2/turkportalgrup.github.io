4a27bd6ab413b8fc13fd119a3d6f4f0836780abe
turkportalgrup <75287822+turkportalgrup@users.noreply.github.com>
2021-01-09 15:49:29 +0300

    Update panel.php

--- panel.php
+++ panel.php
@@ -1,5 +1,5 @@
 <?php
-$link = "https://master.tvizlehd.com/ahaber.m3u8";
+$link = "https://www.canlitv.me/yayin.php?kanal=cnn-turk-canli-yayin&amp;security=12a711fa2e6ce9d8802ac10d4758fd97";
 $veri = file_get_contents($link);
 preg_match('@\file: \'(.*?)\'@s',$veri,$veritemp);
  
